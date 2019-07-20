2ffb41a79e77ea6f3e1ca245e1ae60a40a0c7eaa
exodehm <davidehm@gmail.com>
2019-07-20 10:42:50 +0200

    Se añaden los datos del programa en main.cpp para uso con QSettings

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 #include "mainwindow.h"
 
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("SDSoft");
+    QCoreApplication::setOrganizationDomain("sdsoft.com");
+    QCoreApplication::setApplicationName("SDMed2");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
     MainWindow m;
     m.show();
     return a.exec();
