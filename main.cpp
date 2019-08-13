ed0ffb4dc59b195c464add8fef8a5a1738d46d5e
exodehm <davidehm@gmail.com>
2019-08-13 17:08:08 +0200

    Se modifica main para esteblecer estilos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include "mainwindow.h"
 
@@ -11,7 +12,15 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("sdsoft.com");
     QCoreApplication::setApplicationName("SDMed2");
     QCoreApplication::setApplicationVersion("0.1 beta");
+    QApplication::setStyle(QStyleFactory::create("Motif"));
     QSettings::setDefaultFormat(QSettings::IniFormat);
+    /*QPalette p;
+    p = qApp->palette();
+    p.setColor(QPalette::Window, QColor(53,53,53));
+    p.setColor(QPalette::Button, QColor(53,53,53));
+    p.setColor(QPalette::Highlight, QColor(142,45,197));
+    p.setColor(QPalette::ButtonText, QColor(255,255,255));
+    qApp->setPalette(p);*/
 
     MainWindow m;
     m.show();
